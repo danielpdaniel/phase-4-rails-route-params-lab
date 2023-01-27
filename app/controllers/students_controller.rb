dcad8542087dda229779cb0c6aa2e55081857034
danielpdaniel <artofjquinn@gmail.com>
2023-01-27 17:17:41 -0500

    commit tree 0b8fdfbb56bd5eba6e2beae28385b4edb1bfc16e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = Student.all
+    students = params[:name] ? Student.filter_by_name(params[:name]) : Student.all
     render json: students
+
   end
 
+  def student
+    student = Student.find(params[:id])
+    # searched_student = Student.filter_by_name(params[:name])
+    render json: student
+  end
+
+  # def name_search
+  #   searched_student = Student.all.filter_by_name(params[:search])
+  #   byebug
+  #   render json: searched_student
+  # end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,25 @@
+
+
 class Student < ApplicationRecord
 
   def to_s
     "#{first_name} #{last_name}"
+  end
+
+  def self.filter_by_name(search_term)
+
+    searched_students_array = []
+    self.all.each do |student|
+
+      if student.to_s.upcase === search_term.upcase
+        searched_students_array.push(student)
+      elsif student.first_name.upcase === search_term.upcase
+        searched_students_array.push(student)
+      elsif student.last_name.upcase === search_term.upcase
+        searched_students_array.push(student)
+      end
+    end
+    searched_students_array
   end
 
 end
